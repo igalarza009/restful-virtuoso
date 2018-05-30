3ddad5766b490abb784ece1c4967ae277383904f
Irati Galarza <iratigalarza@hotmail.com>
2018-05-30 19:43:35 +0200

    Pruebas de recogida de file mediante POST completadas

--- src/prueba/service/MyService.java
+++ src/prueba/service/MyService.java
@@ -1,11 +1,19 @@
 package prueba.service;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -23,45 +31,32 @@ import virtuoso.jena.driver.VirtuosoQueryExecutionFactory;
 import virtuoso.jena.driver.VirtuosoUpdateFactory;
 import virtuoso.jena.driver.VirtuosoUpdateRequest;
 
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+
 @Path("/service")
 public class MyService {
 	
 	private String url = "jdbc:virtuoso://localhost:1111";
 
 	@Path("/hello")
-	@GET
-	@Produces("application/json")
-	public Response hello(@QueryParam("name") String name) {
-		String json=null;
-	 	VirtGraph set = new VirtGraph (url, "dba", "dba");
-	 	String consulta = "select distinct ?Concept where {[] a ?Concept} LIMIT 100";
-	 	Query query = QueryFactory.create(consulta);
-	 	VirtuosoQueryExecution qe = VirtuosoQueryExecutionFactory.create(query, set);
-	 	try {
-	 		ResultSet results = qe.execSelect() ;
-	 	    // write to a ByteArrayOutputStream
-	 	    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-	 	    ResultSetFormatter.outputAsJSON(outputStream, results);
-	 	    // and turn that into a String
-	 	    json = new String(outputStream.toByteArray());
-	 	    System.out.println("Guardamos los resultados en variable json");
-	 	 } catch (Exception e) {
-	 	    e.printStackTrace();
-	 	 } finally {
-	 	    qe.close();
-	 	 }
-	 	return Response.ok(json).build();
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello(@FormParam("name") String name) {
+	 	return "Hello " + name;
 	}
 	   
 	@Path("/query")
-	@GET
+//	@GET
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces("application/json")
-	public Response getSPARQLQueryResult_JSON(@QueryParam("query") String consulta){
+	public Response getSPARQLQueryResult_JSON(@FormParam("query") String consulta){ //	@QueryParam
 	 	String json=null;
 	 	VirtGraph set = new VirtGraph (url, "dba", "dba");
 	 	System.out.println("Conexión establecida");
-	 	// -----------FALLA A PARTIR DE AQUÍ -----------
-//	 	String consulta2 = "select distinct ?Concept where {[] a ?Concept} LIMIT 100";
 	 	Query query = QueryFactory.create(consulta);
 	 	VirtuosoQueryExecution qe = VirtuosoQueryExecutionFactory.create(query, set);
 	 	System.out.println("Ejecutamos la consulta");
@@ -84,9 +79,11 @@ public class MyService {
 	   }
 	   
 	   @Path("/insert")
-	   @GET
+//	   @GET
+	   @POST
+	   @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	   @Produces("application/json")//Mirar cómo quitar o modificar esto  
-	   public Response insertRDF(@QueryParam("query") String query){	    
+	   public Response insertRDF(@FormParam("query") String query){	//@QueryParam    
 	 	    //String query = "INSERT DATA {graph <prueba01> {<#book7> <#price> 47 .}}";	    
 	 	    VirtGraph set = new VirtGraph (url, "dba", "dba");
 	 	    VirtuosoUpdateRequest vur = VirtuosoUpdateFactory.create(query, set);
@@ -98,7 +95,7 @@ public class MyService {
 	   @GET
 	   @Produces("application/json")//Mirar cómo quitar o modificar esto   
 	   public Response bulkInsertRDF(@QueryParam("file") String file, @QueryParam("graph") String graph) throws FileNotFoundException{	    
-	    	    String query="", linea;
+	    	String query="", linea;
 	 	    VirtuosoUpdateRequest vur;
 	     
 	 	    VirtGraph set = new VirtGraph (url, "dba", "dba");
@@ -122,5 +119,32 @@ public class MyService {
 
 	 	    return Response.ok().build();
 	   }
+	   
+	   @Path("/insertfile")
+	   @POST
+//	   @GET
+	   @Consumes(MediaType.MULTIPART_FORM_DATA)
+	   @Produces("application/json")//Mirar cómo quitar o modificar esto   
+	   public Response bulkInsertRDF_2(
+			   				@FormDataParam("file") InputStream uploadedInputStream,
+			   				@FormDataParam("file") FormDataContentDisposition fileDetail){
+//		   System.out.println("Hola!!");
+		   try
+		   {
+		       String line;
+		       BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(uploadedInputStream));
+		       while( (line = bufferedReader.readLine()) != null )
+		       { 
+		           System.out.printf("%s\n", line);
+		       }  
+		   } 
+		   catch( IOException e )
+		   {
+		       System.err.println( "Error: " + e );
+		   }
+
+	 	    return Response.ok().build();
+	   }
+	
 	
 }
